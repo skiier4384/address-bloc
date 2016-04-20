5557bd5f7b22e2b244e44a8d677ffca4c7808ed6
Andrew <rothstein.andrew@gmail.com>
2016-04-20 21:38:46 +0000

    Checkpoint 18

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,14 +1,5 @@
  require_relative 'entry'
  require "csv"
- 
- require_relative 'controllers/menu_controller'
- 
- menu = MenuController.new
-
- system "clear"
- puts "Welcome to AddressBloc!"
- 
- menu.main_menu
 
  class AddressBook
    attr_reader :entries
